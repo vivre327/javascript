25795e1c27a0bd6d813ed79c6390f35bc0095c9f
vivre327 <hhaayy27@naver.com>
2024-11-21 20:33:59 +0900

    1주차 과제 테이블1: 게시물 총 갯수 집계

--- 01Content/table1.js
+++ 01Content/table1.js
@@ -1,14 +1,15 @@
 /** 게시물 수를 집계하여 화면에 표시 */
 const updatePostCount = () => {
   const countView = document.querySelector(".count");
-  const tableBody = document.querySelector(".count");
+  const tableBody = document.querySelector(".board tbody");
   const tableRows = tableBody.children.length;
 
   countView.textContent = `총 ${tableRows}개`;
 
   if (tableRows === 0) {
+    console.log("없음");
     tableBody.insertAdjacentHTML("afterbegin", `<tr><td colspan="8">현재 게시물이 존재하지 않습니다.</td></tr>`);
   }
 };
 
-updateGeneralPostCount();
+updatePostCount();
