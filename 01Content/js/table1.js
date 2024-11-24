279454bf89bf99fa98bb5ce294b5653f85cab360
vivre327 <hhaayy27@naver.com>
2024-11-24 13:02:18 +0900

    refactor: 1주차 표1의 게시물 없을 때 colspan 동적 처리로 개선

--- 01Content/js/table1.js
+++ 01Content/js/table1.js
@@ -1,14 +1,17 @@
 /** 게시물 수를 집계하여 화면에 표시 */
 const updatePostCount = () => {
   const countView = document.querySelector(".count");
-  const tableBody = document.querySelector(".board tbody");
-  const tableRows = tableBody.children.length;
+  const table = document.querySelector(".board");
+  const tableBody = table.querySelector("tbody");
+  const tableHeader = table.querySelectorAll("th").length;
+  const tableRows = tableBody.querySelectorAll("tr").length;
+  const noneData = `<tr><td colspan="${tableHeader}">현재 게시물이 존재하지 않습니다.</td></tr>`;
 
   countView.textContent = `총 ${tableRows}개`;
 
   if (tableRows === 0) {
-    console.log("없음");
-    tableBody.insertAdjacentHTML("afterbegin", `<tr><td colspan="8">현재 게시물이 존재하지 않습니다.</td></tr>`);
+    // console.log("없음");
+    tableBody.insertAdjacentHTML("afterbegin", noneData);
   }
 };
 
