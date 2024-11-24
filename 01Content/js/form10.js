0c1395f4166a1301a6d55c0dda8aeec51bb41c77
vivre327 <hhaayy27@naver.com>
2024-11-24 17:21:17 +0900

    refactor: 폼10의 형제요소선택자 대신 data속성선택자 사용

--- 01Content/js/form10.js
+++ 01Content/js/form10.js
@@ -1,6 +1,4 @@
-/* -------------------------------------------------------------------------- */
-/*                                   함수 재사용                               */
-/* -------------------------------------------------------------------------- */
+/** 01.개별 입력 필드의 글자 수 업데이트 */
 // const titleInput = document.getElementById("txt2_1");
 // const titleLog = document.querySelector(".title-count");
 // const contentInput = document.getElementById("txt2_2");
@@ -16,19 +14,28 @@
 
 // updateCount(titleInput, titleLog);
 
-/* -------------------------------------------------------------------------- */
-/*                                  forEach 사용                               */
-/* -------------------------------------------------------------------------- */
+/** 02.클래스명으로 선택한 모든 입력 필드의 글자 수 업데이트 */
 const checkLength = () => {
   const textInputItems = document.querySelectorAll(".input-text");
 
   textInputItems.forEach((item) => {
-    const log = item.nextElementSibling;
+    const id = item.getAttribute("id");
+    const log = document.querySelector(`[data-count-id="${id}"]`);
     const maxLength = item.getAttribute("maxlength");
 
     item.addEventListener("input", (e) => {
-      const count = e.target.value.length;
+      const value = e.target.value;
+      let count = value.length;
+
+      // 한글은 2byte 영어는 1byte 로 표현되므로 maxlength초과하여 입력됨. 제한하는 코드
+      if (count > maxLength) {
+        item.value = value.slice(0, maxLength);
+        count = item.value.length;
+        // console.log("초과됨!");
+      }
+
       log.textContent = `${count}/${maxLength}`;
+      // console.log(count);
     });
   });
 };
