08175a500b9655dcdaccff35629723e23a75cfb3
vivre327 <hhaayy27@naver.com>
2024-12-04 16:07:31 +0900

    다음페이지에서 파라미터로 값을 받아 객체화 시켜 제어하는 location학습

--- 00Basic/16-location-next.js
+++ 00Basic/16-location-next.js
@@ -1,18 +1,35 @@
-console.log(window.location);
+// console.log(window.location);
 
+// 1. window.location.search 콘솔로 조회한다
+// 2. 결과: "?color=blue&size=20" (형:String)
+// 3. URLSearchParams라는 메소드로 객체화 시켜줌
+// 4. 결과 : {color: 'blue', size: '20'} (형: object)
 const params = new URLSearchParams(window.location.search);
+
+// 5. 재할당을 위한 obj빈객체 만들기
 let obj = {};
 
-console.log(params);
-console.log(typeof params);
+// console.log(params);
+// console.log(typeof params);
 
-// params.forEach((value, key) => {
-//   console.log(value, key);
-//   obj[key] = value; //객체에 key를 추가하고 그에 대한 value을 설정한다.
-// });
+// 6. 객체화된 데이터 접근은 반드시!! forEach로!!
+params.forEach((value, key) => {
+  // console.log(value, key);
+  obj[key] = value; //객체에 key를 추가하고 그에 대한 value을 설정한다.
+  // 7. 결과: blue color , 20 size
+});
 
-for (const [key, value] of params) {
-  obj[key] = value;
-}
+// 객체 등록 방식 2type
+// A. 점 표기법 : obj.key = value; -> 점 표기법은 동적생성이 아닌, 이미 존재하는 객체 접근할때
+// B. 대괄호 표기법 : obj[key] = value; => 동적으로 생성된 obj에 객체 추가,접근 할때.
+
+// for (const [key, value] of params) {
+//   obj[key] = value;
+// }
 
 console.log(obj);
+
+document.querySelector("h2").style.cssText = `
+  color:${obj.color};
+  font-size:${obj.size}px;
+`;
--- 00Basic/16-location.js
+++ 00Basic/16-location.js
@@ -9,7 +9,7 @@ const links = document.querySelectorAll("a");
 /*                                   현재페이지정보                            */
 /* -------------------------------------------------------------------------- */
 links[0].addEventListener("click", () => {
-  console.log(url);
+  console.log(window.location);
 });
 
 /* -------------------------------------------------------------------------- */
@@ -17,8 +17,12 @@ links[0].addEventListener("click", () => {
 /* -------------------------------------------------------------------------- */
 prev.addEventListener("click", (event) => {
   event.preventDefault();
-  console.log(location.pathname);
-  url.assign("16-location-next.html");
+  // console.log(location.pathname);
+  // 1. assign 방식 : window.location.assign("이동할 페이지명")
+  window.location.assign("16-location-next.html");
+  // 2. location.href 방식 : ="이동할 페이지명"
+  // ****두 방식 같은 개념. 둘다 history가 추가되어 뒤로가기가 가능하다!!
+  // location.href = "#ddd";
 });
 
 /* -------------------------------------------------------------------------- */
@@ -33,6 +37,28 @@ prev2.addEventListener("click", (event) => {
 /*                                   강력새로고침                              */
 /* -------------------------------------------------------------------------- */
 reload.addEventListener("click", () => {
-  location.reload();
-  // location.reload(true);
+  // 1. 일반 새로고침
+  // location.reload(); F5
+  // 2. 강력 새로고침 : 쿠키,캐시데이터 모두 삭제된다. (ctrl+shift+F5)
+  location.reload(true);
 });
+
+/* -------------------------------------------------------------------------- */
+/*                                   파라미터                              */
+/* -------------------------------------------------------------------------- */
+// [예시] 네이버 블로그 URL
+// https://section.blog.naver.com/BlogHome.naver?directoryNo=0&currentPage=1&groupId=0
+
+// 파라미터: ?물음표(시작) key=value 한 쌍과 &구분자의 반복
+// ?directoryNo=0&currentPage=1&groupId=0
+
+// 객체로 표현해보면 아래와 같다
+// const obj = {
+//   directoryNo: 0,
+//   currentPage: 1,
+//   groupId: 0,
+// };
+
+// **Why, 파라미터를 넘기는 이유?
+// 위와 같은 key value 값을 이전페이지에서 전달해주면
+// 다음페이지가 받아서 처리할 수 있기 때문.
