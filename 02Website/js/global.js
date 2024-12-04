ab5e7f1be34b4a23e451b0e293e5bb8f13d79ca6
vivre327 <hhaayy27@naver.com>
2024-12-05 00:11:56 +0900

    알람리스트 바인딩 기능 추가

--- 02Website/js/global.js
+++ 02Website/js/global.js
@@ -1,5 +1,8 @@
 import { includeHTML } from "./ui/includeHTML.js";
 import { footerYear } from "./ui/footerYear.js";
+import { alarmBind } from "./ui/alarmBind.js";
+import { alarmData } from "./data/alarmData.js";
+// import { mapTest } from "./ui/mapTest.js";
 
 // include
 // 그려진 뒤에 조작해야하므로 가장 상단에 위치
@@ -7,4 +10,7 @@ includeHTML("/02Website/include/header.html", ".header");
 includeHTML("/02Website/include/footer.html", ".footer").then(() => {
   // footerYear
   footerYear();
+  // alarm
+  alarmBind(alarmData);
+  // mapTest();
 });
