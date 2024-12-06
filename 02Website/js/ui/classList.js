3e39e589d82e956c1055517dc5fe9ad1d3f1006b
vivre327 <hhaayy27@naver.com>
2024-12-06 21:39:33 +0900

    바인딩 수정

--- 02Website/js/ui/classList.js
+++ 02Website/js/ui/classList.js
@@ -8,23 +8,19 @@ export const classList = (arry) => {
     return;
   }
 
-  arry.forEach((item) => {
+  const classItems = arry.map((item) => {
     // 객체 구조분해할당
     const { thumbnail, category, coinPrice, title, tags, format, period, applyPeriod } = item;
 
-    // li생성후 ul에 추가
-    const li = document.createElement("li");
-    el(".list-type1").append(li);
-
     // li에 데이터바인딩
-    li.innerHTML = /* html */ `
+    return /* html */ `
       <a href="">
         <div class="thumbnail">
           <img src=${thumbnail} alt="" />
         </div>
         <div class="badges">
           <span class="badge ${category === "경영" ? "orange" : "purple"}">${category}</span>
-          <span class="badge coin">${coinPrice}</span>
+          <span class="badge coin">${coinPrice.toLocaleString()}원</span>
         </div>
         <strong class="list-type1__title">${title}</strong>
         <div class="hash">
@@ -44,6 +40,7 @@ export const classList = (arry) => {
         </ul>
         <div class="list-type1__date">신청기간 : ${applyPeriod}</div>
       </a>
-    `;
+      `;
   });
+  el(".list-type1").innerHTML = classItems.join("");
 };
