fde1c8a3315fa925b0f70c9948af19fd6e9c99cc
vivre327 <hhaayy27@naver.com>
2024-12-06 20:28:34 +0900

    파일 첨부 기능 수정

--- 02Website/js/ui/fileInfo.js
+++ 02Website/js/ui/fileInfo.js
@@ -3,15 +3,25 @@ import { el } from "../helper.js";
 export const fileInfo = () => {
   const applyFile = el("#fileInput");
   const fileView = el(".file-meta");
-  const fileName = fileView.querySelector(".file-name");
-  const fileSize = fileView.querySelector(".file-size");
 
   applyFile.addEventListener("change", (e) => {
-    const fileInfo = e.target.files;
-    // console.log(typeof fileInfo);
-    console.log(fileInfo);
-    console.log(fileInfo[0].size);
-    fileName.textContent = fileInfo[0].name;
-    fileSize.textContent = fileInfo[0].size;
+    const fileMeta = e.target.files;
+    // console.log(typeof fileMeta);
+    // console.log(fileMeta);
+    // console.log(fileMeta[0].size);
+    const p = document.createElement("p");
+    p.textContent = `${fileMeta[0].name} (${fileMeta[0].size} Bytes)`;
+    fileView.append(p);
   });
+
+  // 파일사이즈 변환 함수 추가하기! Math.floor
+  /**
+   * Bytes 바이트
+   * KB 킬로바이트
+   * MB 메가바이트
+   * GB 기가바이트
+   * TB 테라바이트
+   */
+  // 파일의 단위값:  1byte=210 = 1024
+  // const unit = ["Bytes", "KB", "MB", "GB", "TB"];​
 };
